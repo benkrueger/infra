f5823580365d6232b63c7611f64ea019eadf143e
benjamin krueger <benjamin.oliver.krueger@gmail.com>
2023-09-17 14:53:42 -0500

    c2s

--- scripts/get_docker_ufw.sh
+++ scripts/get_docker_ufw.sh
@@ -1,3 +1,6 @@
-#!/bin/dash
-sudo wget -O /usr/local/bin/ufw-docker https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker;
-sudo chmod +x /usr/local/bin/ufw-docker;
+  - name: Download ufw-docker
+    become: yes
+    get_url:
+      url: https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker
+      dest: /usr/local/bin/ufw-docker
+      mode: '0755'
